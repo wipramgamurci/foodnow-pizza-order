bf8b2c6d0fb3b9ccd1e901f3f2e38262ad6210ce
Wipram Gamurci <wipramgamurci@gmail.com>
2025-02-21 22:58:20 +0800

    Create pizza card component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
-import Card from "./components/Card.vue";
-import Button from "./components/Button.vue";
 import Header from "./components/Header.vue";
+import PizzaCard from "./components/PizzaCard.vue";
+
+import PizzaList from "./data/pizza-list.json";
+
+const pizzaList = PizzaList.data;
 </script>
 
 <template>
@@ -9,13 +12,26 @@ import Header from "./components/Header.vue";
   <div class="hero">
     <p>Pizza order</p>
   </div>
-  <Card />
-  <Button>Click me</Button>
+  <div class="container">
+    <div class="left">
+      <h1>Chose your pizza</h1>
+      <div class="pl">
+        <PizzaCard v-for="pizza in pizzaList" :key="pizza.id" :pizza="pizza" />
+      </div>
+      <h1>Custom pizza</h1>
+    </div>
+    <div class="right">right</div>
+  </div>
   <div class="filler"></div>
 </template>
 
 <style lang="scss" scoped>
 @use "./styles/_mixins.scss";
+
+.pl {
+  display: flex;
+  gap: 30px;
+}
 
 .filler {
   height: 100vh;
@@ -32,6 +48,21 @@ import Header from "./components/Header.vue";
 
   p {
     @include mixins.text-8xl;
+    font-weight: 700;
+    color: var(--primary-color);
+  }
+}
+
+.container {
+  width: 85vw;
+  margin: 160px auto;
+  // @include mixins.x-center-absolute;
+
+  display: flex;
+  gap: 100px;
+
+  h1 {
+    @include mixins.text-5xl;
     font-weight: 700;
     color: var(--primary-color);
   }
---
+++ src/components/PizzaCard.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+defineProps(["pizza"]);
+</script>
+
+<template>
+  <div class="pizza-card">
+    <img src="./../assets/Cheese Pizza.png" alt="" />
+    <div>
+      <p class="title">{{ pizza.name }}</p>
+      <div class="price">
+        <p v-if="pizza.discount.is_active">${{ pizza.discount.final_price }}</p>
+        <p :class="{ strike: pizza.discount.is_active }">${{ pizza.price }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use "./../styles/_mixins.scss";
+
+.pizza-card {
+  padding: 32px;
+  border-radius: 8px;
+  background-color: var(--text-white);
+  border: 1px solid #3333331a;
+  display: flex;
+  gap: 32px;
+  align-items: center;
+  transition: background-color 0.5s ease;
+
+  img {
+    transition: transform 0.5s ease;
+  }
+
+  &:hover {
+    background-color: #e77e234d;
+
+    img {
+      transform: rotate(20deg);
+    }
+  }
+
+  .price {
+    display: flex;
+    gap: 8px;
+  }
+
+  p {
+    @include mixins.text-xl;
+
+    &.title {
+      font-weight: 700;
+    }
+
+    &.strike {
+      text-decoration: line-through;
+      color: rgb(149, 149, 148);
+    }
+  }
+}
+</style>
